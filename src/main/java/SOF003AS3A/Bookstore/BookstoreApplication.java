ed9cf69799639dc674abf7232a3bd7575b09906f
Joonas Montonen <joonas.montonen@outlook.com>
2023-02-12 13:29:50 +0200

    Added listpage function

--- src/main/java/SOF003AS3A/Bookstore/BookstoreApplication.java
+++ src/main/java/SOF003AS3A/Bookstore/BookstoreApplication.java
@@ -1,5 +1,7 @@
 package SOF003AS3A.Bookstore;
 
+import java.util.List;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,7 +9,8 @@ import org.springframework.context.annotation.Bean;
 
 import SOF003AS3A.Bookstore.domain.Book;
 import SOF003AS3A.Bookstore.domain.BookRepositery;
-import jakarta.persistence.Column;
+
+
 
 
 @SpringBootApplication
@@ -21,14 +24,19 @@ public class BookstoreApplication {
 	@Bean
 	public CommandLineRunner demoData (BookRepositery bookRepositery) {
 		return (arg) -> {
-			Book book1 = new Book(0, "Joo", "Joo-mies", 1823, "aaaa", 200);
-	    	bookRepositery.save(book1); // SQL Insert
-	       
-	
+			  Book book1 = new Book(1, "Eläinten vallankumous", "George Orwell", 1945, "9780151002177", 12);
+			  Book book2 =new Book(2,"Rikos ja rangaistus", "Fjodor Dostojevski", 1866 , "9788726021226", 34);
+			  bookRepositery.save(book1); // SQL Insert
+			  bookRepositery.save(book2);    
+	   	
+  	List<Book> books = (List<Book>)bookRepositery.findAll();
+	       for (Book book: books) {
+	        System.out.println(book.toString());
 		
 	         
 		
 		
 		};
+};
 }
 }
--- src/main/java/SOF003AS3A/Bookstore/web/BookController.java
+++ src/main/java/SOF003AS3A/Bookstore/web/BookController.java
@@ -2,12 +2,21 @@ package SOF003AS3A.Bookstore.web;
 
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import SOF003AS3A.Bookstore.domain.Book;
 import SOF003AS3A.Bookstore.domain.BookRepositery;
+
+
+
 
 
 
@@ -28,4 +37,24 @@ public class BookController {
 	
 		return "Bookstore";
 }
+
+
+
+@RequestMapping("/booklist")
+public String bookList (Model model) {
+	
+	model.addAttribute("books", bookRepositery.findAll());
+	
+	
+	return "booklist";
+	
+	
 }
+
+
+
+
+	
+	}
+
+
